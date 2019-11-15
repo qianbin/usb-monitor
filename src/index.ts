8fb326460b451e10c90d7a408df4a31ae213ffd9
qianbin <cola.tin.com@gmail.com>
2019-11-15 14:22:46 +0800

    debounce 'change' event

--- src/index.ts
+++ src/index.ts
@@ -9,25 +9,28 @@ export class UsbMonitor extends EventEmitter {
     constructor(readonly vendorId?: number, readonly productId?: number) {
         super()
 
-        const handleAttach = (device: USB.Device) => {
+        let timer: any
+        const emitChange = (device: USB.Device) => {
             if (!this.isMatchedDevice(device.deviceDescriptor.idVendor, device.deviceDescriptor.idProduct)) {
                 return
             }
-            this.emit(UsbMonitor.CHANGE_EVENT)
-        }
-        const handleDetach = (device: USB.Device) => {
-            if (!this.isMatchedDevice(device.deviceDescriptor.idVendor, device.deviceDescriptor.idProduct)) {
-                return
+
+            // debounce
+            if (timer) {
+                clearTimeout(timer)
             }
-            this.emit(UsbMonitor.CHANGE_EVENT)
+            timer = setTimeout(() => {
+                timer = undefined
+                this.emit(UsbMonitor.CHANGE_EVENT)
+            }, 500)
         }
 
-        USB.on('attach', handleAttach)
-        USB.on('detach', handleDetach)
+        USB.on('attach', emitChange)
+        USB.on('detach', emitChange)
 
         this._close = () => {
-            USB.removeListener('attach', handleAttach)
-            USB.removeListener('detach', handleDetach)
+            USB.removeListener('attach', emitChange)
+            USB.removeListener('detach', emitChange)
         }
     }
 
